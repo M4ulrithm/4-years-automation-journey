91f8739770d2aa433f6026784249603616db4ca6
Maul999 <lanadikara@gmail.com>
2026-06-27 22:02:36 +0700

    data-types

--- month-6/week-1/01-learn-data-types.py
+++ month-6/week-1/01-learn-data-types.py
@@ -1,17 +1,3 @@
-# ==========================================
-# PROYEK: Automation Journey (BPA)
-# MATERI: Pengenalan Tipe Data Dasar Python
-# ==========================================
-
-# 1. Integer (Bilangan Bulat)
-panjang = 10  # Menyimpan angka bulat tanpa koma
-
-# 2. Float (Bilangan Pecahan/Desimal)
-lebar = 5.5   # Menyimpan angka desimal (menggunakan titik)
-
-# 3. String (Teks)
-nama_project = "BPA"  # Menyimpan teks, wajib diapit tanda kutip
-
-# Kalkulasi dan Output
-# Python otomatis mengalikan Integer dengan Float menjadi Float (55.0)
-print(panjang * lebar)
+#Exponentation
+a = 2 ** 3
+print (a)
